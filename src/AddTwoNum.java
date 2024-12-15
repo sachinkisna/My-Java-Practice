d483ae4deddfc520cc166bf7d3452cdbbd733831
Sachinkisna <sachinsoftware0001@gmail.com>
2024-12-15 12:57:01 +0530

    Initial Commit

---
+++ src/AddTwoNum.java
@@ -0,0 +1,7 @@
+public class AddTwoNum {
+    public static void main(String[] args) {
+        int num1=34;
+        int num2=45;
+        System.out.println("sum is "+(num2+num1));
+    }
+}
---
+++ src/Hello_Krishna.java
@@ -0,0 +1,5 @@
+public class Hello_Krishna{
+    public static void main(String[] args){
+        System.out.println("Hello Krishna");
+    }
+}
